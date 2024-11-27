05a6b4e8fa2591b67337bc6b41048418a3f1c14f
Ali Arefi <ali.are@mtnirancell.com>
2024-11-27 14:01:35 +0330

    add logger

--- app/tasks/translation_task.py
+++ app/tasks/translation_task.py
@@ -2,8 +2,10 @@ from faststream.rabbit import RabbitBroker, RabbitMessage
 from faststream import FastStream
 from config import RABBITMQ_HOST, RESPONSE_QUEUE
 from models.transcription import TranscriptionDocument
-import json
+import json, logging
 from uuid import UUID
+
+logger = logging.getLogger(__name__)
 
 def queue_listener():
     broker = RabbitBroker(url=f"amqp://{RABBITMQ_HOST}")
@@ -12,6 +14,7 @@ def queue_listener():
     @broker.subscriber(RESPONSE_QUEUE)
     async def process_translation_request(message: RabbitMessage):
         try:
+            logger.info('Receive new translation')
             message_data = message.body
             message_json = json.loads(message_data)
             text_to_translate = message_json.get("translated_text", "")
@@ -31,5 +34,5 @@ def queue_listener():
             await transcription.save()
 
         except Exception as e:
-            print(f"Error processing translation request: {e}")
+            logger.error(f"Error processing translation request: {e}")
     return app, broker
